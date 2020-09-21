5f9161b035f47535e6ab497cffb5fc21f7247e1c
RePierre <petru.rebeja@gmail.com>
2020-09-21 17:50:55 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ro_benchmark_leaderboard/precache-manifest.98e6846f1490b272fc2987690493475f.js"
+  "/ro_benchmark_leaderboard/precache-manifest.19639e5274f95bfd550991d9f9218d7e.js"
 );
 
 self.addEventListener('message', (event) => {
